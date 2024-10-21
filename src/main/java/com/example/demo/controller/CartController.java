b7e6d9c57187e743172477cbe73df7ff9e363911
Nikilhari <nikilhari3@gmail.com>
2024-10-21 13:46:53 +0530

    cart functions completed

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,4 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Cart;
+import com.example.demo.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/{email}")
+    public ResponseEntity<List<Cart>> getCartItemsByEmail(@PathVariable String email)
+    {
+        List<Cart> cartItems = cartService.getItemsByEmail(email);
+        return ResponseEntity.ok(cartItems);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Cart> addItems(@RequestBody Cart cart)
+    {
+        Cart savedItems = cartService.addOrUpdateCart(cart);
+        return ResponseEntity.ok(savedItems);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id)
+    {
+        cartService.removeCartItem(id);
+        return ResponseEntity.ok("Item removed successfully");
+    }
 }
--- src/main/java/com/example/demo/services/CartService.java
+++ src/main/java/com/example/demo/services/CartService.java
@@ -1,4 +1,25 @@
 package com.example.demo.services;
 
+import com.example.demo.model.Cart;
+import com.example.demo.repository.CartRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
 public class CartService {
+@Autowired
+    private CartRepo cartRepo;
+    public List<Cart> getItemsByEmail(String email) {
+        return cartRepo.findByEmail(email);
+    }
+
+    public Cart addOrUpdateCart(Cart cart) {
+        return cartRepo.save(cart);
+    }
+
+    public void removeCartItem(Long cartId) {
+        cartRepo.deleteById(cartId);
+    }
 }
